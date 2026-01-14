3245bb888c8e63fbbaae6d6f77aba7b4962a97f3
KAMONPORN\Latthidech-PC <kamonporn.t@datanow.co.th>
2026-01-14 15:25:44 +0700

    add oauth

--- src/server.ts
+++ src/server.ts
@@ -20,13 +20,77 @@ const routes = [
   ...packageUsageRoutes,
 ];
 
-// Dummy implementations for googleLogin and googleCallback
 function googleLogin() {
-  return new Response("Google Login Endpoint", { status: 200 });
+  const params = new URLSearchParams({
+    client_id: process.env.GOOGLE_CLIENT_ID!,
+    redirect_uri: "http://localhost:3000/auth/google/callback",
+    response_type: "code",
+    scope: "openid email profile",
+    prompt: "select_account",
+  });
+
+  return Response.redirect(
+    "https://accounts.google.com/o/oauth2/v2/auth?" + params.toString(),
+    302
+  );
 }
-function googleCallback(url: URL) {
-  return new Response("Google Callback Endpoint", { status: 200 });
+
+async function googleCallback(url: URL) {
+  const code = url.searchParams.get("code");
+  if (!code) {
+    return new Response("No code", { status: 400 });
+  }
+
+  const tokenRes = await fetch("https://oauth2.googleapis.com/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({
+      code,
+      client_id: process.env.GOOGLE_CLIENT_ID!,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET!,
+      redirect_uri: "http://localhost:3000/auth/google/callback",
+      grant_type: "authorization_code",
+    }),
+  });
+
+  type GoogleTokenResponse = {
+  access_token: string;
+  id_token: string;
+  expires_in: number;
+  token_type: string;
+};
+
+  const token = (await tokenRes.json()) as GoogleTokenResponse;
+
+  // const token = await tokenRes.json();
+  const { id_token } = token;
+
+  const payload = await verifyGoogleToken(id_token);
+
+  // payload คือ user จาก Google
+  // { sub, email, name, picture }
+
+  return new Response(
+    JSON.stringify({
+      user: payload,
+    }),
+    { headers: { "Content-Type": "application/json" } }
+  );
 }
+
+async function verifyGoogleToken(idToken: string) {
+  const { payload } = await jwtVerify(idToken, GOOGLE_JWKS, {
+    issuer: "https://accounts.google.com",
+    audience: process.env.GOOGLE_CLIENT_ID,
+  });
+
+  return payload;
+}
+
+
+// function googleCallback(url: URL) {
+//   return new Response("Google Callback Endpoint", { status: 200 });
+// }
 
 Bun.serve({
   port: 3000,
